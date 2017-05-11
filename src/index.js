0dc5246e45639a4dd398e69c5768e4ae8e6d59b2
João Amaral <jamaral@pasonatquila.com>
2017-05-11 15:51:59 +0900

    search bar with debounce (from lodash)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 // create a new react component that produces some HTML
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
@@ -29,9 +30,10 @@ class App extends Component {
   }
 
   render(){
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 1000);
     return (
       <div>
-        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+        <SearchBar onSearchTermChange={videoSearch} />
         <VideoDetail video={this.state.selectedVideo}/>
         <VideoList
           onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }
